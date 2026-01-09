129c956aa0e1766b152c4829d39a57e7dea15212
fertrevino <fernando.yanez.trevino@gmail.com>
2026-01-10 00:44:07 +0100

    apply precommit

--- src/cloud_eval/verifiers_run.py
+++ src/cloud_eval/verifiers_run.py
@@ -6,10 +6,6 @@ from typing import Dict, Type
 from .verifier import Verifier
 from tasks.aws.s3.simple_bucket.verify import S3BucketVerifier
 from tasks.aws.s3.application_logs.verify import S3ApplicationLogsVerifier
-from tasks.aws.s3.backups_bucket.verify import S3BackupsBucketVerifier
-from tasks.aws.s3.set_bucket_private.verify import S3SetBucketPrivateVerifier
-from tasks.aws.sqs.create_queue.verify import SQSQueueVerifier
-from tasks.aws.sns.create_topic.verify import SNSTopicVerifier
 
 # Map of task_id to verifier implementation
 VERIFIERS: Dict[str, Type[Verifier]] = {
