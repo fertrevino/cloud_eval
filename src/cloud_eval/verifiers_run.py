09409170b84b9a48a8508e75abd50eb18c69892c
fertrevino <fernando.yanez.trevino@gmail.com>
2026-01-10 00:51:26 +0100

    remove commented line

--- src/cloud_eval/verifiers_run.py
+++ src/cloud_eval/verifiers_run.py
@@ -11,8 +11,6 @@ from tasks.aws.s3.set_bucket_private.verify import S3SetBucketPrivateVerifier
 from tasks.aws.sqs.create_queue.verify import SQSQueueVerifier
 from tasks.aws.sns.create_topic.verify import SNSTopicVerifier
 
-#
-
 # Map of task_id to verifier implementation
 VERIFIERS: Dict[str, Type[Verifier]] = {
     "cloud-eval-s3-simple-bucket": S3BucketVerifier,
